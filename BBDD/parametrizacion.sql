34c4d6cfb3050da080d7b207d880c36eb082dea7
Maria Orrantia Garcia <a27912@svalero.com>
2024-05-18 11:29:19 +0200

    cambios url

--- BBDD/parametrizacion.sql
+++ BBDD/parametrizacion.sql
@@ -9,9 +9,9 @@ CREATE OR REPLACE PROCEDURE SCRIPT_INICIALIZACION_TABLAS
 		INSERT INTO Products(name, price, stock, image_url)
 		VALUES ('Pencil', 0.5, 80, 'https://www.collinsdictionary.com/images/full/pencil_280523090.jpg');
 		INSERT INTO Products(name, price, stock, image_url)
-		VALUES ('Eraser', 1.5, 50, 'https://i.ebayimg.com/images/g/DX4AAOxyRNJScXF-/s-l1200.jpg');
+		VALUES ('Eraser', 1.5, 50, 'https://thumb.pccomponentes.com/w-530-530/articles/29/291688/1603-milan-goma-de-borrar-430-colores-surtidos-1-unidad.jpg');
 		INSERT INTO Products(name, price, stock, image_url)
-		VALUES ('Pencil Sharpener', 2, 15, 'https://i.ebayimg.com/images/g/j24AAOSwxuha~AV-/s-l1200.jpg');
+		VALUES ('Pencil Sharpener', 2, 15, 'https://m.media-amazon.com/images/I/41cHKfMAnXL.__AC_SY300_SX300_QL70_ML2_.jpg');
 		INSERT INTO Products(name, price, stock, image_url)
 		VALUES ('Fountain Pen', 0.5, 80, 'https://www.penboutique.com/cdn/shop/files/Montblanc-Meisterstuck-Fountain-Pen-149-Black-Red-Gold-Trim-Montblanc-42473361.jpg?v=1708824108');
 	
